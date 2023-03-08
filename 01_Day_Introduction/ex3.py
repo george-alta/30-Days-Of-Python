c2e9c521b601005d1b69b2518695a3a0a2611422
Jorge <jorge.altamirano@gmail.com>
2023-03-09 06:08:29 +1300

    new file:   01_Day_Introduction/ex3.py 	new file:   03_Day_Operators/ex1.py

---
+++ 01_Day_Introduction/ex3.py
@@ -0,0 +1,6 @@
+import math
+distance = ((10 - 8) ** 2 + (3 - 8) ** 2 ) ** 1/2
+print(distance)
+
+circle_area = 3 ** 2 * math.pi
+print(circle_area)
---
+++ 03_Day_Operators/ex1.py
@@ -0,0 +1,9 @@
+age = int()
+height = float()
+co = complex()
+
+#triangle area
+trianglebase = input("enter base:")
+triangleheight = input("enter height:")
+triangle_area =  int(trianglebase) * int(triangleheight) / 2
+print("the area is:",triangle_area)
